3a638e1d34e99d0eb48c7db4089b8ef9b311fefe
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-12 12:04:17 +0530

    Delete videlytics_project directory

--- videlytics_project/wsgi.py
+++
@@ -1,8 +0,0 @@
-# Web Server Gateway Interface (WSGI) config for videlytics_project project.
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'videlytics_project.settings')
-
-application = get_wsgi_application()
