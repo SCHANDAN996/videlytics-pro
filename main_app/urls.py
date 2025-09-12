31ff52a678a4debd00f8d414e35fcb9dcb751470
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-12 09:49:14 +0530

    Update urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path
+from .views import home_view, register_view, login_view, logout_view, analyze_channel_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('accounts/signup/', views.signup, name='signup'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', home_view, name='home'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    # Naya logout URL
+    path('logout/', logout_view, name='logout'),
+    path('api/analyze_channel/', analyze_channel_view, name='analyze_channel'),
 ]
