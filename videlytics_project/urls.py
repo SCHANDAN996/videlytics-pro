1987fc4dbf6e5af099b556796ba32184513d70bb
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-12 09:47:23 +0530

    Update urls.py

--- videlytics_project/urls.py
+++ videlytics_project/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')), # Hamari main_app ke URLs ko yahan jodein
+    # main_app ke URLs ko include karein
+    path('', include('main_app.urls')),
+    # Django ke default auth URLs (login, logout, etc.) ko include karein
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
