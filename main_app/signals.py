9f6d65cb2b15d262eae18697ed9c91b5cc84c830
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-14 04:47:19 +0530

    Update signals.py

--- main_app/signals.py
+++ main_app/signals.py
@@ -1,16 +1,15 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Wallet
+from .models import UserProfile
 
 @receiver(post_save, sender=User)
-def create_wallet(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile.
+    If a user is created, a new UserProfile is created.
+    """
     if created:
-        Wallet.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_wallet(sender, instance, **kwargs):
-    try:
-        instance.wallet.save()
-    except AttributeError:
-        Wallet.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
+    # Ensure the profile is saved whenever the user is saved.
+    instance.profile.save()
