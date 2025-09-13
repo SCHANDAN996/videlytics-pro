8d84e7ada001883c5bcc70d6aaa4b280ef9977ed
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-14 04:47:59 +0530

    Update admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Wallet, Transaction
+from .models import Plan, UserProfile, Subscription
 
-admin.site.register(Wallet)
-admin.site.register(Transaction)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'razorpay_plan_id')
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'referral_code', 'referral_credits', 'referred_by')
+    search_fields = ('user__username',)
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'start_date', 'end_date', 'is_active')
+    list_filter = ('plan', 'is_active')
+    search_fields = ('user__username',)
+
+admin.site.register(Plan, PlanAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
