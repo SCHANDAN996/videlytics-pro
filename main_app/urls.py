1e0808faf40a484b51495013860b6edc4be80e97
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-13 13:09:58 +0530

    Update urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world_view, name='hello_world'), # Sirf ek URL
+    path('', views.home_view, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
