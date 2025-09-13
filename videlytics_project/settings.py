88390a464596790aea07d85900ec0e395d4b62db
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-13 12:52:09 +0530

    Update settings.py

--- videlytics_project/settings.py
+++ videlytics_project/settings.py
@@ -1,24 +1,14 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-import dj_database_url
-
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-# DEBUG ko production mein False rakhein
-DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1', 't']
+# Sabse zaroori: DEBUG ko True rakhein taaki humein error dikhe
+DEBUG = True 
 
-ALLOWED_HOSTS = []
-# Render ke domain ko automatically jodein
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-# Apne custom domains bhi jodein
-ALLOWED_HOSTS.extend(['www.videlytics.pro', 'videlytics.pro', '127.0.0.1'])
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key-for-debugging')
 
+ALLOWED_HOSTS = ['*'] # Debugging ke liye sabko allow karein
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -26,14 +16,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic', # Whitenoise ko jodein
     'django.contrib.staticfiles',
     'main_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # Whitenoise ko yahan bhi jodein
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -49,50 +37,15 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
     },
 ]
 
-WSGI_APPLICATION = 'videlytics_project.wsgi.application'
-
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES = { 'default': dj_database_url.config(conn_max_age=600, conn_health_checks=True) }
-else:
-    DATABASES = { 'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3' } }
-
-
-AUTH_PASSWORD_VALIDATORS = [
-    { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
-    { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
-    { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
-    { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
-]
-
-
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
-
-STATIC_URL = 'static/'
-# Production ke liye zaroori settings
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
+STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_URL = 'login' 
-LOGIN_REDIRECT_URL = 'home'
