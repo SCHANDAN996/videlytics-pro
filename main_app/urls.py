af8124ebcf5773e6085c7ebb3c7eddb98ae1f8f8
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-13 15:11:03 +0530

    Update urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('wallet/', views.wallet_view, name='wallet'),
+    path('api/create_order/', views.create_order_view, name='create_order'),
+    path('payment/handler/', views.payment_handler_view, name='payment_handler'),
+    path('api/analyze_channel/', views.analyze_channel_view, name='analyze_channel'),
 ]
