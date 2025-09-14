71b88a70ebe3c7a8ef092756263b8bc68461111f
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-14 07:34:23 +0530

    Update settings.py

--- videlytics_project/settings.py
+++ videlytics_project/settings.py
@@ -8,13 +8,20 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# DEBUG को False ही रहने दें, लेकिन एरर देखने के लिए अस्थायी रूप से True कर सकते हैं
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1', 't']
 
+# --- यहाँ बदलाव करें ---
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-ALLOWED_HOSTS.extend(['www.videlytics.pro', 'videlytics.pro', '127.0.0.1'])
+
+# अपने कस्टम डोमेन को यहाँ जोड़ें
+ALLOWED_HOSTS.extend(['videlytics.pro', 'www.videlytics.pro'])
+# --- बदलाव समाप्त ---
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -22,13 +29,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic', # Whitenoise static files
     'django.contrib.staticfiles',
     
     # My Apps
     'main_app',
     
-    # Third-party Apps
+    # 3rd Party Apps
     'crispy_forms',
     'crispy_tailwind',
     'allauth',
@@ -37,95 +44,4 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
-]
-
-ROOT_URLCONF = 'videlytics_project.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], # Add this line
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'videlytics_project.wsgi.application'
-
-# Database
-if 'DATABASE_URL' in os.environ:
-    DATABASES = { 'default': dj_database_url.config(conn_max_age=600, conn_health_checks=True) }
-else:
-    DATABASES = { 'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3' } }
-
-# Password validators
-AUTH_PASSWORD_VALIDATORS = [
-    { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
-    { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
-    { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
-    { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
-]
-
-# Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
-# Static files
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STORAGES = {
-    "staticfiles": { "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage", },
-}
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# --- ALLAUTH SETTINGS ---
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
-SITE_ID = 1
-LOGIN_REDIRECT_URL = '/dashboard/'
-LOGOUT_REDIRECT_URL = '/'
-ACCOUNT_EMAIL_VERIFICATION = "optional" # or "mandatory"
-ACCOUNT_AUTHENTICATION_METHOD = "username_email"
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_USERNAME_REQUIRED = True
-ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True
-ACCOUNT_SESSION_REMEMBER = True
-ACCOUNT_LOGOUT_ON_GET = True # Allows logout without confirmation page
-
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': ['profile', 'email'],
-        'AUTH_PARAMS': {'access_type': 'online'},
-        'APP': {
-            'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
-            'secret': os.environ.get('GOOGLE_SECRET_KEY'),
-            'key': ''
-        }
-    }
-}
-
-# --- CRISPY FORMS SETTINGS ---
-CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
-CRISPY_TEMPLATE_PACK = "tailwind"
+# ... (बाकी फ़ाइल वैसी ही रहेगी) ...
