f798e062035c8b323924938a684c9559f49e2221
SCHANDAN996 <work.chandansingh@gmail.com>
2025-09-14 11:59:18 +0530

    Update forms.py

--- main_app/forms.py
+++ main_app/forms.py
@@ -4,18 +4,28 @@ from crispy_forms.layout import Layout, Submit
 
 class CustomLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
-        super(CustomLoginForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_method = 'post'
         self.helper.form_class = 'space-y-6'
-        self.helper.field_template = 'tailwind/field.html'
-        self.helper.add_input(Submit('submit', 'Sign In', css_class='w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'))
+        # यहाँ हम इस फ़ॉर्म को बता रहे हैं कि Tailwind का इस्तेमाल करो
+        self.helper.template_pack = "tailwind" 
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            'login',
+            'password',
+            'remember'
+        )
 
 class CustomSignupForm(SignupForm):
     def __init__(self, *args, **kwargs):
-        super(CustomSignupForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_method = 'post'
         self.helper.form_class = 'space-y-6'
-        self.helper.field_template = 'tailwind/field.html'
-        self.helper.add_input(Submit('submit', 'Sign Up', css_class='w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'))
+         # यहाँ हम इस फ़ॉर्म को बता रहे हैं कि Tailwind का इस्तेमाल करो
+        self.helper.template_pack = "tailwind"
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            'email',
+            'password',
+            'password2'
+        )
